dadaa4c558c508222f79daf0ad100d9bf3729636
Katherine Bayard <katherinebayard@zipcoders-MBP-6.lan>
2019-02-06 23:06:37 -0500

    Number Utilities complete

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,59 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String numList = "";
+        for (int i=0; i<stop; i++) {
+         numList = numList + i;   
+        }
+        return numList;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String numList = "";
+        for (int i=start; i<stop; i++) {
+         numList = numList + i;   
+        }
+        return numList;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String numList = ("" + start);
+        int current = start;
+        do {
+         numList = numList + (current + step);  
+         current = current + step;
+        } while (current < stop - step);
+        return numList;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String numList = "";
+        for (int i=start; i<stop; i++) {
+            if(i % 2 == 0) {
+                numList = numList + i;
+            }
+        }
+        return numList;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String numList = "";
+        for (int i=start; i<stop; i++) {
+            if(i % 2 != 0) {
+                numList = numList + i;
+            }
+        }
+        return numList;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String numList = "";
+        for (int i=start; i<=stop; i++) {
+            numList = numList + (int) Math.pow(i, exponent);
+        }
+        return numList;
     }
 }
