f583aada5bb408ac9e94dd0cff228722e2883165
Katherine Bayard <katherinebayard@zipcoders-MacBook-Pro-6.local>
2019-02-07 13:36:17 -0500

    add completed Table Utilities

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,38 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String result = "";
+        for(int i=1; i<=5; i++) {
+            for(int j=1; j<=5; j++) {
+                result = result + String.format("% 3d |",(j * i));   
+            }   
+            result = result + "\n";
+        }
+        
+        return result;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String result = "";
+        for(int i=1; i<=10; i++) {
+            for(int j=1; j<=10; j++) {
+                result = result + String.format("%3d |",(j * i));   
+            }   
+            result = result + "\n";
+        }
+        
+        return result;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+        for(int i=1; i<=tableSize; i++) {
+            for(int j=1; j<=tableSize; j++) {
+                result = result + String.format("%3d |",(j * i));   
+            }   
+            result = result + "\n";
+        }
+        
+        return result;
     }
 }
